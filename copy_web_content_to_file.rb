28fd6280cc6d1774be766afdbf87b1d1c01c4a69
Nolan Neeley <nolan@Nolans-MacBook-Air.local>
2018-01-23 10:07:20 -0600

    Added readme

--- copy_web_content_to_file.rb
+++ copy_web_content_to_file.rb
@@ -1,8 +1,3 @@
-#Write a script that accesses a wikipedia page and copies it to an html file on your hard drive.
-#Possible uses: proactively run script to save an article locally for reading when offline and unable to access the web. 
-#Exercise taken from: http://ruby.bastardsbook.com/chapters/io/
-
-
 require 'rubygems'
 require 'rest-client'
 
